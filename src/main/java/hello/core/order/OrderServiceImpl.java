b48d37364a8d01ae39e43465dc344078e640e6b0
Donghun0425 <yeom6460@naver.com>
2026-06-25 11:39:23 +0900

    Spring Bean 예제 작성

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,9 +7,22 @@ import hello.core.member.*;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // 역할과 구현은 분리하였지만, 객체지향 설계원칙을 충실히 준수하지 못했다.
+    // DIP : 주문서비스 (OrderServiceImpl)은 DiscountPolicy 인터페이스에 의존하면서 구현체 클래스에도 의존하고 있음.
+    // OCP : 지금 코드의 기능 [Repository (DB,Memory), DiscountPolicy (Fix, Rate)]을 변경하면 클라이언트 코드에 영향을 준다.
+    // -> OCP 위반 ( 예- 할인정책 변경 시 OrderServiceImpl 코드 수정해야 함.)
+    // -> 해결책 : 추상에만 의존하도록 변경 (인터페이스에만 의존하도록) / 누군가 구현객체를 대신 생성하고 주입하도록 변경
+    //private final MemberRepository memberRepository;
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,16 +1,42 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderServiceTest {
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    void BeforeEach(){
+//        AppConfig appconfig = new AppConfig();
+//        memberService = appconfig.memberService();
+//        orderService = appconfig.orderService();
+        memberService = ac.getBean("memberService", MemberService.class);
+        orderService = ac.getBean("orderService", OrderService.class);
+    }
+
+    @Test
+    @DisplayName("모든 Bean 출력하기")
+    void findAllBean() {
+        String[] beanDifinitionNames = ac.getBeanDefinitionNames();
+
+        for(String value : beanDifinitionNames){
+            Object bean = ac.getBean(value);
+            System.out.println("name = " + beanDifinitionNames + ", object = " + bean);
+        }
+    }
 
     @Test
     void createOrder (){
@@ -18,7 +44,7 @@ public class OrderServiceTest {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
